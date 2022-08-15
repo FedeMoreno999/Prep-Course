f9c4e3910f5d26b635a4efa049817e52131a38e1
Fede <morenofede521@gmail.com>
2022-08-15 16:41:15 -0300

    sexto comiteo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -181,7 +181,7 @@ function mesesDelAño(array) {
   // Tu código:
   var nuevoArray = [];
   for(let i= 0; i<array.length; i++){
-    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre");{
+    if(array[i] === "Enero" || array[i] === "Diciembre" || array[i] === "Marzo" || array[i] === "Noviembre");{
      nuevoArray.push(array[i]);
    }
   }
