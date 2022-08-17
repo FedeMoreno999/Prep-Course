8487ef3d55d302565f073290594a2318312d7899
Fede <morenofede521@gmail.com>
2022-08-17 19:42:53 -0300

    commit jsiv

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -131,6 +131,16 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var usuario = {
+    posts: [{
+      likes:4
+    }]
+  };
+  var suma = 0;
+  for(var i = 0; i < usuario.posts.length; i++){
+    suma = suma + usuario.posts[i].likes;
+  }
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento (producto) {
@@ -143,6 +153,15 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  var producto = {
+    precio: 10,
+    porcentajeDeDescuento: 5,
+    calcularPrecioDescuento:
+  };
+  producto.calcularPrecioDescuento = function(){
+    return this.precio - (this.precio * this.porcentajeDeDescuento);
+  }
+  return producto;
 
 }
 
