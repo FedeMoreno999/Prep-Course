bd7a3dcc4ac9d5cfa305dd05f5ea67fabb246262
Fede <morenofede521@gmail.com>
2022-08-17 19:36:19 -0300

    commit jsiv

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -79,8 +79,8 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(objeto["propiedad"]){
-    return true;
+  if(objeto[key] = "propiedad"){
+   return true;
   } else {
     return false;
   }
